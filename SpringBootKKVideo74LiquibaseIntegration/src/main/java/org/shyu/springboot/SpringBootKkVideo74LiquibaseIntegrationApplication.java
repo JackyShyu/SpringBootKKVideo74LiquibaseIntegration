0476618aa74a7ea8b6463eb6595292c2a1402a71
jackyshyu <jackyshyu@gmail.com>
2018-10-05 11:45:14 -0500

    Implemented project

---
+++ SpringBootKKVideo74LiquibaseIntegration/src/main/java/org/shyu/springboot/SpringBootKkVideo74LiquibaseIntegrationApplication.java
@@ -0,0 +1,12 @@
+package org.shyu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKkVideo74LiquibaseIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKkVideo74LiquibaseIntegrationApplication.class, args);
+	}
+}
